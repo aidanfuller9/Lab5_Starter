db3c82950f5cb0608705ae546d1731d34ad4827d
MaximeVergnet <maxime.vrgnt@gmail.com>
2026-05-04 21:13:26 -0700

    updated selectro

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,14 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const selector = document.getElementById("horn-select");
+  const image = document.querySelector("img")
+  const audio = document.querySelector("audio")
+
+  selector.addEventListener("change", updateHorn);
+
+  function updateHorn(e) {
+    image.src = `assets/images/${event.target.value}.svg`
+    audio.src = `assets/audio/${event.target.value}.mp3`
+  }
 }
