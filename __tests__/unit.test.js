05feb5d87df6bbcc8b0d27a1942ad12afae36d7e
MaximeVergnet <maxime.vrgnt@gmail.com>
2026-05-05 13:06:49 -0700

    added unit tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -23,15 +23,29 @@ test('Checks 56-7890 to be incorrect', () => {
     expect(isPhoneNumber("56-7890")).toBe(false);
 });
 
+// isPhoneNumber (false)
+test('Checks 556a-7890 to be incorrect', () => {
+    expect(isPhoneNumber("556a-7890")).toBe(false);
+});
+
 // isEmail (true)
 test('is aifuller@ucsd.edu a valid email', () => {
   expect(isEmail("aifuller@ucsd.edu")).toBe(true);
 });
 
+// isEmail (true)
+test('is a@a.aa a valid email', () => {
+  expect(isEmail("a@a.aa")).toBe(true);
+});
 
 //isEmail (false)
 test('is ai.fuller@ucsd.edu a valid email', () => {
   expect(isEmail("ai.fuller@ucsd.edu")).toBe(false);
+});
+
+//isEmail (false)
+test('is a@aa a valid email', () => {
+  expect(isEmail("a@aa")).toBe(false);
 });
 
 // isStrongPassword (true)
@@ -39,10 +53,19 @@ test('is iL0v3Pr0gr4mming a strong password', () => {
   expect(isStrongPassword("iL0v3C0ding")).toBe(true);
 });
 
+// isStrongPassword (true)
+test('is aaaa a strong password', () => {
+  expect(isStrongPassword("aaaa")).toBe(true);
+});
 
 // isStrongPassword (false)
 test('is 1lovecoding a strong password', () => {
   expect(isStrongPassword("1lovecoding")).toBe(false);
+});
+
+// isStrongPassword (false)
+test('is aaaaaaaaaaaaaaaaaaaaaaaaaa a strong password', () => {
+  expect(isStrongPassword("aaaaaaaaaaaaaaaaaaaaaaaaaa")).toBe(false);
 });
 
 // isDate (true)
@@ -50,8 +73,37 @@ test('is 05/04/2026 a valid date', () => {
   expect(isDate("05/04/2026")).toBe(true);
 });
 
+// isDate (true)
+test('is 5/4/2026 a valid date', () => {
+  expect(isDate("5/4/2026")).toBe(true);
+});
 
 // isDate (false)
 test('is 007/04/2006 a valid date', () => {
   expect(isDate("007/04/2006")).toBe(false);
+});
+
+// isDate (false)
+test('is 007/04/206 a valid date', () => {
+  expect(isDate("007/04/206")).toBe(false);
+});
+
+// isHexColor (true)
+test('is 00ff55 a HexColor', () => {
+  expect(isHexColor("00ff55")).toBe(true);
+});
+
+// isHexColor (true)
+test('is 00f a HexColor', () => {
+  expect(isHexColor("00f")).toBe(true);
+});
+
+// isHexColor (false)
+test('is 00hf55 a HexColor', () => {
+  expect(isHexColor("00hf55")).toBe(false);
+});
+
+// isHexColor (false)
+test('is 00ff5 a HexColor', () => {
+  expect(isHexColor("00ff5")).toBe(false);
 });
