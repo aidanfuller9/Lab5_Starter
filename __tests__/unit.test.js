a87a27e48261280d70b67226edbc19fbf9c3fbdc
Aidan Fuller <aidanfuller321@gmail.com>
2026-05-04 23:47:03 -0700

    unit tests for password, email, date

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,30 +8,50 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// isPhoneNumber
+// isPhoneNumber (true)
 test('Checks 123-456-7890 to be correct', () => {
     expect(isPhoneNumber("123-456-7890")).toBe(true);
 });
 
+// isPhoneNumber (true)
 test('Checks (123)456-7890 to be correct', () => {
     expect(isPhoneNumber("(123)456-7890")).toBe(true);
 });
 
+// isPhoneNumber (false)
 test('Checks 56-7890 to be incorrect', () => {
     expect(isPhoneNumber("56-7890")).toBe(false);
 });
 
-test('Checks (123)456-789011 to be incorrect', () => {
-    expect(isPhoneNumber("(123)456-789011")).toBe(false);
-});
-
-// isEmail
+// isEmail (true)
 test('is aifuller@ucsd.edu a valid email', () => {
   expect(isEmail("aifuller@ucsd.edu")).toBe(true);
 });
 
-//isDate
 
-test(' a valid email', () => {
-  expect(isEmail("aifuller@ucsd.edu")).toBe(true);
+//isEmail (false)
+test('is ai.fuller@ucsd.edu a valid email', () => {
+  expect(isEmail("ai.fuller@ucsd.edu")).toBe(false);
+});
+
+// isStrongPassword (true)
+test('is iL0v3Pr0gr4mming a strong password', () => {
+  expect(isStrongPassword("iL0v3C0ding")).toBe(true);
+});
+
+
+// isStrongPassword (false)
+test('is 1lovecoding a strong password', () => {
+  expect(isStrongPassword("1lovecoding")).toBe(false);
+});
+
+// isDate (true)
+test('is 05/04/2026 a valid date', () => {
+  expect(isDate("05/04/2026")).toBe(true);
+});
+
+
+// isDate (false)
+test('is 007/04/2006 a valid date', () => {
+  expect(isDate("007/04/2006")).toBe(false);
 });
